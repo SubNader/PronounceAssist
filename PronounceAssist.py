8b37d186b8b81e673ec6568c712c87e6d0f503a0
Nader Alsharkawy <naderalsharkawy@gmail.com>
2016-12-15 22:00:39 +0200

    Linux support using ffplay for playback

--- PronounceAssist.py
+++ PronounceAssist.py
@@ -1,16 +1,29 @@
 from gtts import gTTS
 from playsound import playsound
+import os
 
 
 # Convert text to speech using gTTS
 def say(message,selected_language):
     speech = gTTS(text=message, lang=selected_language)
     speech.save("audio/temp.mp3")
-    playsound("audio/temp.mp3")
+    play("audio/temp.mp3")
+
+
+# Play file with cross-platform handling
+def play(path):
+    try:
+        playsound(path)
+    except:
+        try:
+            os.system("ffplay -nodisp -nostats -hide_banner -loglevel error -autoexit "+ path)
+        except:
+            print "Sorry, your operating system is not supported yet."
+            exit(0)
 
 # Welcome message
 print "Hi, I'm PronounceAssist!"
-playsound("audio/welcome.mp3")
+play("audio/welcome.mp3")
 
 # Set language (English / Arabic)
 language_input = raw_input("Enter 'English' or 'Arabic': ").lower()
@@ -26,4 +39,3 @@ while True:
     message = raw_input("Text: ")
     if message != '':
         say(message,language)
-
